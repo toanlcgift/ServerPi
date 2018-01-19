e00ceb5ce327263d8b36c0724d850ab1fb04e4f3
Nguyễn Đức Toàn <toanndse02449@fpt.edu.vn>
2018-01-20 03:04:16 +0700

    update

--- MPUTest/MPUTest.ino
+++ MPUTest/MPUTest.ino
@@ -1,55 +1,93 @@
-/*
- Name:		MPUTest.ino
- Created:	1/20/2018 12:19:14 AM
- Author:	ToanND
-*/
-#include "Wire.h"
-#define A_SCALE (16384.0)
-const int MPU_addr = 0x68;
-int16_t AcX, AcY, AcZ, Tmp, GyX, GyY, GyZ;
-float pitch, roll, yaw;
-
-float dist(float a, float b)
+#include<Wire.h>
+const int MPU = 0x68;
+int t = 0, dt = 1;
+int AcX, AcY, AcZ, GyX, GyY, GyZ, tmp;
+int AcXo, AcYo, AcZo, GyXo, GyYo, GyZo;
+float roll = 0, pitch = 0, rollgy = 0, pitchgy = 0, rollac = 0, pitchac = 0, Ax, Ay, Az, Gx, Gy, Gz, gain = 0.95;
+void MPUconfig(int Addr, int data)
 {
-	return sqrt((a*a) + (b*b));
+	Wire.beginTransmission(MPU);
+	Wire.write(Addr);
+	Wire.write(data);
+	Wire.endTransmission();
 }
-
-void setup_mpu() {
-	Wire.begin(D1, D2);
-	Wire.beginTransmission(MPU_addr);
-	Wire.write(0x6B); // 0x6B: PWR_MGMT_1 register name (datasheet)
-	Wire.write(0);
-	Wire.endTransmission(true);
-}
-
-// the setup function runs once when you press reset or power the board
-void setup() {
-	Serial.begin(9600);
-	setup_mpu();
-}
-
-// the loop function runs over and over again until power down or reset
-void loop() {
-	Wire.beginTransmission(MPU_addr);
-	Wire.write(0x3B); //0x3B: ACCEL_XOUT_H register name (datasheet)
-	Wire.endTransmission(false);
-	Wire.requestFrom(MPU_addr, 14, 1);
-	AcX = Wire.read() << 8 | Wire.read(); // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)
-	AcY = Wire.read() << 8 | Wire.read(); // 0x3D ... & 0x3E ...
-	AcZ = Wire.read() << 8 | Wire.read(); //...
-	Tmp = Wire.read() << 8 | Wire.read();
-	GyX = Wire.read() << 8 | Wire.read();
-	GyY = Wire.read() << 8 | Wire.read();
+void MPUread()
+{
+	Wire.beginTransmission(MPU);
+	Wire.write(0x3B);  // starting with register 0x3B (ACCEL_XOUT_H)
+	Wire.endTransmission();
+	Wire.requestFrom(MPU, 14);  // request a total of 14 registers
+	AcX = Wire.read() << 8 | Wire.read();  // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)    
+	AcY = Wire.read() << 8 | Wire.read();  // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
+	AcZ = Wire.read() << 8 | Wire.read();  // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
+	tmp = Wire.read() << 8 | Wire.read();  // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)    
+	GyX = Wire.read() << 8 | Wire.read();  // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
+	GyY = Wire.read() << 8 | Wire.read();  // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
 	GyZ = Wire.read() << 8 | Wire.read();
-	if (AcX == -1 && AcY == -1 && AcZ == -1 && GyX == -1 && GyY == -1 && GyZ == -1)
-		return;
-	float radians1 = atan2(-AcX / A_SCALE, dist(AcY / A_SCALE, AcZ / A_SCALE));
-	pitch = radians1 * 180 / 3.14;
+}
+void offset()
+{
+	MPUconfig(0x1A, 0b00000000);
+	int ax = 0, ay = 0, az = 0, gx = 0, gy = 0, gz = 0, i;
+	MPUread();
+	MPUread();
+	ax = AcX;
+	ay = AcY;
+	az = AcZ;
+	gx = GyX;
+	gy = GyY;
+	gz = GyZ;
+	for (i = 0; i<1100; i++)
+	{
+		MPUread();
+		if (i>100)
+		{
+			ax = (ax + AcX) / 2;
+			ay = (ay + AcY) / 2;
+			az = (az + AcZ) / 2;
+			gx = (gx + GyX) / 2;
+			gy = (gy + GyY) / 2;
+			gz = (gz + GyZ) / 2;
+		}
+		delay(2);
+	}
+	AcXo = ax;
+	AcYo = ay;
+	AcZo = 16384 - az;
+	GyXo = gx;
+	GyYo = gy;
+	GyZo = gz;
+	MPUconfig(0x1A, 0b00000110);
+}
+void setup()
+{
+	Serial.begin(115200);
+	Wire.begin(D1, D2);
+	MPUconfig(0x1A, 0b00000110);
+	MPUconfig(0x1B, 0b00000000);
+	MPUconfig(0x1C, 0b00000000);
+	MPUconfig(0x6B, 0b00000000);
+	offset();
+}
 
-	roll = atan2(AcY / A_SCALE, AcZ / A_SCALE) * 180 / 3.14;
-
-	float radians3 = atan2(AcZ / A_SCALE, dist(AcX / A_SCALE, AcY / A_SCALE));
-	yaw = radians3 * 180 / 3.14;
-	printf("degrees: %f ----- %f ----- %f", pitch, roll, yaw);
-	Serial.println(Tmp / 340.00 + 36.53);
+void loop()
+{
+	t = millis();
+	MPUread();
+	Ax = (float)(AcX - AcXo) / 16384;
+	Ay = (float)(AcY - AcYo) / 16384;
+	Az = (float)(AcZ - AcZo) / 16384;
+	Gx = (float)(GyX - GyXo) / 131;
+	Gy = (float)(GyY - GyYo) / 131;
+	Gz = (float)(GyZ - GyZo) / 131;
+	pitchgy = (Gy * ((float)dt / 1000)) + pitch;
+	rollgy = (Gx * ((float)dt / 1000)) + roll;
+	pitchac = atan2(Ax, Az) * (float)(180 / PI);
+	rollac = atan2(Ay, Az) * (float)(180 / PI);
+	roll = gain * rollgy + (1 - gain)* rollac;
+	pitch = gain * pitchgy + (1 - gain) * pitchac;
+	Serial.print(roll);
+	Serial.print(" "); Serial.print(pitch);
+	Serial.println(" ");
+	dt = millis() - t;
 }
