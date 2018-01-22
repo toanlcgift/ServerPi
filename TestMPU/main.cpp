11316317424afea2b8d2eb5b26ff24f2ed381d39
Nguyễn Đức Toàn <toanndse02449@fpt.edu.vn>
2018-01-22 17:25:49 +0700

    test hmc5883l

--- TestMPU/main.cpp
+++ TestMPU/main.cpp
@@ -1,90 +1,52 @@
-/*
-MPU6050 Interfacing with Raspberry Pi
-http://www.electronicwings.com
-*/
-
-#include <wiringPiI2C.h>
+#include <stdint.h>
 #include <stdlib.h>
 #include <stdio.h>
-#include <wiringPi.h>
+#include <unistd.h>
 #include <math.h>
+#include <wiringPi.h>
+#include <wiringPiI2C.h>
+#include "hmc5883l.h"
 
-#define Device_Address 0x68	/*Device Address/Identifier for MPU6050*/
+#define PI 3.14159265
 
-#define PWR_MGMT_1   0x6B
-#define SMPLRT_DIV   0x19
-#define CONFIG       0x1A
-#define GYRO_CONFIG  0x1B
-#define INT_ENABLE   0x38
-#define ACCEL_XOUT_H 0x3B
-#define ACCEL_YOUT_H 0x3D
-#define ACCEL_ZOUT_H 0x3F
-#define GYRO_XOUT_H  0x43
-#define GYRO_YOUT_H  0x45
-#define GYRO_ZOUT_H  0x47
-int t = 0, dt = 1;
-
-int fd;
-
-void MPU6050_Init() {
-
-	wiringPiI2CWriteReg8(fd, SMPLRT_DIV, 0x07);	/* Write to sample rate register */
-	wiringPiI2CWriteReg8(fd, PWR_MGMT_1, 0x01);	/* Write to power management register */
-	wiringPiI2CWriteReg8(fd, CONFIG, 0);		/* Write to Configuration register */
-	wiringPiI2CWriteReg8(fd, GYRO_CONFIG, 24);	/* Write to Gyro Configuration register */
-	wiringPiI2CWriteReg8(fd, INT_ENABLE, 0x01);	/*Write to interrupt enable register */
-
-}
-short read_raw_data(int addr) {
-	short high_byte, low_byte, value;
-	high_byte = wiringPiI2CReadReg8(fd, addr);
-	low_byte = wiringPiI2CReadReg8(fd, addr + 1);
-	value = (high_byte << 8) | low_byte;
-	return value;
-}
-
-int main() {
-
-	float Acc_x, Acc_y, Acc_z;
-	float Gyro_x, Gyro_y, Gyro_z;
-	float Ax = 0, Ay = 0, Az = 0;
-	float Gx = 0, Gy = 0, Gz = 0;
-	float roll = 0, pitch = 0, rollgy = 0, pitchgy = 0, rollac = 0, pitchac = 0, gain = 0.95;
-	fd = wiringPiI2CSetup(Device_Address);   /*Initializes I2C with device Address*/
-	MPU6050_Init();		                 /* Initializes MPU6050 */
-
-	while (1)
-	{
-		t = millis();
-		/*Read raw value of Accelerometer and gyroscope from MPU6050*/
-		Acc_x = read_raw_data(ACCEL_XOUT_H);
-		Acc_y = read_raw_data(ACCEL_YOUT_H);
-		Acc_z = read_raw_data(ACCEL_ZOUT_H);
-
-		Gyro_x = read_raw_data(GYRO_XOUT_H);
-		Gyro_y = read_raw_data(GYRO_YOUT_H);
-		Gyro_z = read_raw_data(GYRO_ZOUT_H);
-
-		/* Divide raw value by sensitivity scale factor */
-		Ax = Acc_x / 16384.0;
-		Ay = Acc_y / 16384.0;
-		Az = Acc_z / 16384.0;
-
-		Gx = Gyro_x / 131;
-		Gy = Gyro_y / 131;
-		Gz = Gyro_z / 131;
-
-		printf("\n Gx=%.3f °/s\tGy=%.3f °/s\tGz=%.3f °/s\tAx=%.3f g\tAy=%.3f g\tAz=%.3f g\n", Gx, Gy, Gz, Ax, Ay, Az);
-		//delay(500);
-		pitchgy = (Gy * ((float)dt / 1000)) + pitch;
-		rollgy = (Gx * ((float)dt / 1000)) + roll;
-		pitchac = atan2(Ax, Az) * (float)(180 / 3.14);
-		rollac = atan2(Ay, Az) * (float)(180 / 3.14);
-		roll = gain * rollgy + (1 - gain)* rollac;
-		pitch = gain * pitchgy + (1 - gain) * pitchac;
-		printf("roll: %f \n pitch: %f\n", roll, pitch);
-		dt = millis() - t;
-
+void checkRC(int rc, char *text) {
+	if (rc < 0) {
+		printf("Error: %s - %d\n", text, rc);
+		exit(-1);
 	}
+}
+
+int main(int argc, char *argv[]) {
+	printf("HMC5883L starting\n");
+	wiringPiSetupSys();
+	// Setup Wiring Pi
+	wiringPiSetup();
+
+	// Open an I2C connection
+	int fd = wiringPiI2CSetup(HMC5883L_ADDRESS);
+	checkRC(fd, "wiringPiI2CSetup");
+
+	// Perform I2C work
+	wiringPiI2CWriteReg8(fd, HMC5883L_REG_MODE, HMC5883L_MODE_CONTINUOUS);
+
+	while (1) {
+		uint8_t msb = wiringPiI2CReadReg8(fd, HMC5883L_REG_MSB_X);
+		uint8_t lsb = wiringPiI2CReadReg8(fd, HMC5883L_REG_LSB_X);
+		short x = msb << 8 | lsb;
+
+		msb = wiringPiI2CReadReg8(fd, HMC5883L_REG_MSB_Y);
+		lsb = wiringPiI2CReadReg8(fd, HMC5883L_REG_LSB_Y);
+		short y = msb << 8 | lsb;
+
+		msb = wiringPiI2CReadReg8(fd, HMC5883L_REG_MSB_Z);
+		lsb = wiringPiI2CReadReg8(fd, HMC5883L_REG_LSB_Z);
+		short z = msb << 8 | lsb;
+
+		double angle = atan2((double)y, (double)x) * (180 / PI) + 180;
+
+		printf("x=%d, y=%d, z=%d - angle=%f\n", x, y, z, angle);
+		sleep(1);
+	}
+
 	return 0;
 }
