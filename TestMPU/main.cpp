78219205c5d2a3a848c19dd9dc25b88dfd1d6d7f
Nguyễn Đức Toàn <toanndse02449@fpt.edu.vn>
2018-01-24 23:24:41 +0700

    update

--- TestMPU/main.cpp
+++ TestMPU/main.cpp
@@ -1,23 +1,60 @@
 #include <stdio.h>  
 #include <wiringPiI2C.h>  
 #include <wiringPi.h>  
+#include "QMC5883L.h"
+#include <math.h>
+
+DFRobot_QMC5883 compass;
+
+void loop() {
+	Vector norm = compass.readNormalize();
+
+	// Calculate heading
+	float heading = atan2(norm.YAxis, norm.XAxis);
+
+	// Set declination angle on your location and fix heading
+	// You can find your declination on: http://magnetic-declination.com/
+	// (+) Positive or (-) for negative
+	// For Bytom / Poland declination angle is 4'26E (positive)
+	// Formula: (deg + (min / 60.0)) / (180 / M_PI);
+	float declinationAngle = (4.0 + (26.0 / 60.0)) / (180 / PI);
+	heading += declinationAngle;
+
+	// Correct for heading < 0deg and heading > 360deg
+	if (heading < 0) {
+		heading += 2 * PI;
+	}
+
+	if (heading > 2 * PI) {
+		heading -= 2 * PI;
+	}
+
+	// Convert to degrees
+	float headingDegrees = heading * 180 / M_PI;
+
+	// Output
+	printf(" Heading = %f, Degrees = f",heading, headingDegrees);
+
+	delay(100);
+}
 int main()
 {
-	int fd_Magnetic = wiringPiI2CSetup(0x0d);
-	if (fd_Magnetic < 0)
+	while (!compass.begin())
 	{
-		printf("Error Setup wiringPi I2C!\n");
-	}
-	wiringPiI2CWriteReg8(fd_Magnetic, 0x09, 0x1d);
-	while (1)
-	{
-		printf("%x ", wiringPiI2CReadReg8(fd_Magnetic, 0x00));
-		printf("%x ", wiringPiI2CReadReg8(fd_Magnetic, 0x01));
-		printf("%x ", wiringPiI2CReadReg8(fd_Magnetic, 0x02));
-		printf("%x ", wiringPiI2CReadReg8(fd_Magnetic, 0x03));
-		printf("%x ", wiringPiI2CReadReg8(fd_Magnetic, 0x04));
-		printf("%x \n", wiringPiI2CReadReg8(fd_Magnetic, 0x05));
+		printf("Could not find a valid QMC5883 sensor, check wiring!");
 		delay(500);
+	}
+
+	if (compass.isQMC()) {
+		printf("Initialize QMC5883");
+		compass.setRange(QMC5883_RANGE_2GA);
+		compass.setMeasurementMode(QMC5883_CONTINOUS);
+		compass.setDataRate(QMC5883_DATARATE_50HZ);
+		compass.setSamples(QMC5883_SAMPLES_8);
+	}
+	while (true)
+	{
+		loop();
 	}
 	return 0;
 }
