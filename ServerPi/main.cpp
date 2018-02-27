73da9cee30b481f7c4aa58ad9a409afb16ab2879
Nguyễn Đức Toàn <toanndse02449@fpt.edu.vn>
2018-02-28 00:07:48 +0700

    update

--- ServerPi/main.cpp
+++ ServerPi/main.cpp
@@ -38,18 +38,17 @@ int main ()
     return 1 ;
   }
 
-  softServoSetup (0, 1, 2, 3, 4, 5, 6, 7) ;
+  softServoSetup (0, 2, 3, 12, 13, 14, 5, 6) ;
 
-  softServoWrite (0,  90) ;
-/*
-  softServoWrite (1, 1000) ;
-  softServoWrite (2, 1100) ;
-  softServoWrite (3, 1200) ;
-  softServoWrite (4, 1300) ;
-  softServoWrite (5, 1400) ;
-  softServoWrite (6, 1500) ;
-  softServoWrite (7, 2200) ;
-*/
+  softServoWrite (0, 750) ;
+  softServoWrite (1, 750) ;
+  softServoWrite (2, 750) ;
+  softServoWrite (3, 750) ;
+  softServoWrite (4, 750) ;
+  softServoWrite (5, 750) ;
+  softServoWrite (6, 750) ;
+  softServoWrite (7, 750) ;
+
 
   for (;;)
     delay (10) ;
